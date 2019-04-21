0e29458a6b8193949a6521d21685e4adc132528c
Amr Shabory <amr.shabory@gmail.com>
2019-04-21 10:31:43 +0200

    uploading the plugin

---
+++ HasoubSeo.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Plugin Name: Hasoub SEO 
+Plugin URI: https://academy.hsoub.com/
+Description: wordpress plugin you can use to optimize your website for search engines and social media websites .
+Author: Hasoub CO
+Author URI: https://academy.hsoub.com/
+Version: 1.0
+Text Domian:hasoub-seo
+Domain Path: /languages
+*/
+wp_enqueue_script('custom-js', plugins_url( '', __FILE__ ).'/js/custom-js.js');
+wp_enqueue_script('limit', plugins_url( '', __FILE__ ).'/js/limit.js');
+wp_enqueue_script('selectall', plugins_url( '', __FILE__ ).'/js/selectall.js');
+wp_enqueue_script('mytabs', plugins_url( '', __FILE__ ).'/js/mytabs.js',array( 'jquery-ui-tabs' ));
+wp_enqueue_script('jquery-ui', plugins_url('', __FILE__) . '/js/jquery-ui.js');
+wp_enqueue_script('nashr-autocomplete', plugins_url('', __FILE__) . '/js/nashr-autocomplete.js', array( 'jquery-ui-autocomplete' ));
+
+//load ajax files
+wp_register_script("ajax-js", plugins_url( '', __FILE__ ) . '/js/ajax-js.js', array('jquery'));
+wp_localize_script('ajax-js', 'ajax', array('ajaxurl' => admin_url('admin-ajax.php')));
+wp_enqueue_script('ajax-js');
+
+// load ajax
+wp_register_script("ajax-nashr",plugins_url('', __FILE__) .'/js/ajax-nashr.js', array('jquery'));
+wp_localize_script('ajax-nashr', 'myAjax', array('ajaxurl' => admin_url('admin-ajax.php')));
+wp_enqueue_script('ajax-nashr');
+
+load_plugin_textdomain('mnbaa-seo', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+include( plugin_dir_path( __FILE__ ) . 'includes/mnbaa_functions.php');
+include( plugin_dir_path( __FILE__ ) . 'controllers/ajax_functions.php');
+include( plugin_dir_path( __FILE__ ) . 'includes/wp_functions.php');
+include( plugin_dir_path( __FILE__ ) . 'includes/arrays.php');
+register_activation_hook( __FILE__, 'mnbaa_seo_activate' );
+mnbaa_seo_run_plugin();
+
+
+?>
